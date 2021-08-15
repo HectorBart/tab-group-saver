34f0220229f4550206f9cfcd457788f83babe12b
Hector Robe <hector.brobe@gmail.com>
2021-08-15 23:45:58 +0100

    Completed save functionality.

--- popup.js
+++ popup.js
@@ -1,26 +1,122 @@
+function createBookmarkParent() {
+
+    return (chrome.bookmarks.create({title: "TabGroupSaver"}));
+
+}
+
+function getBookmarkParent() {
+
+    return new Promise((resolve, reject) => {
+
+        chrome.bookmarks.getTree().then(bookmarkTree => {
+            
+            var otherBookmarks = bookmarkTree[0].children.find(child => child.id == "2");
+            var bookmarkParent = otherBookmarks.children.find(child => child.title == "TabGroupSaver");
+
+            if(bookmarkParent == null) {
+
+                createBookmarkParent().then(newBookmarkParent => {
+
+                    resolve(newBookmarkParent);
+
+                });
+
+            } else {
+
+                resolve(bookmarkParent);
+
+            }
+
+        });
+
+    });
+
+}
+
+function createBookmarkGroup(groupName, groupId) {
+
+    return new Promise((resolve, reject) => {
+
+        getBookmarkParent().then(bookmarkParent => {
+
+            chrome.bookmarks.create({parentId: bookmarkParent.id, title: groupName + " - " + groupId}).then(bookmarkGroup => {
+
+                resolve(bookmarkGroup);
+
+            })
+
+        });
+        
+    });
+
+}
+
+function createTabBookmark(tab, bookmarkGroupId) {
+
+    return (chrome.bookmarks.create({parentId: bookmarkGroupId, title: tab.title, url: tab.url}));
+
+}
+
 function getAllGroups() {
 
     return (chrome.tabGroups.query({}));
 
 }
 
+function getGroup(groupId) {
+
+    return (chrome.tabGroups.get(parseInt(groupId)));
+
+}
+
 function getAllTabsInGroup(groupId) {
 
-    console.log(parseInt(groupId));
+    // Not working
+    // return (chrome.tabs.query({
+    //     groupId: parseInt(groupId)
+    // }));
 
-    return (chrome.tabs.query({
-        groupId: parseInt(groupId)
-    }));
+    return new Promise((resolve, reject) => {
+
+        chrome.tabs.query({}).then(tabs => {
+
+            var tabsInGroup = tabs.filter(tab => tab.groupId == groupId);
+
+            resolve(tabsInGroup);
+
+        });
+
+    });
 
 }
 
 function saveGroup() {
 
-    getAllTabsInGroup(this.id).then(tabs => {
+    return new Promise((resolve, reject) => {
 
-        tabs.forEach(tab => {
-            
-            console.log(tab);
+        getGroup(this.id).then(group => {
+
+            createBookmarkGroup(group.title, group.id).then(bookmarkGroup => {
+
+                getAllTabsInGroup(group.id).then(tabs => {
+
+                    tabs.forEach(tab => {
+                        
+                        createTabBookmark(tab, bookmarkGroup.id).then(tabBookmark => {
+
+                            chrome.tabs.remove(tab.id);
+
+                        });
+        
+                    });
+                    
+                    location.reload();
+        
+                    resolve();
+
+                });
+
+            })        
 
         });
 
