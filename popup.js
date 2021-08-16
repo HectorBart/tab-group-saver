b47253936863edac5714d88d5877b1adba39c44d
Hector Robe <hector.brobe@gmail.com>
2021-08-16 23:36:59 +0100

    Completed restore group functionality

--- popup.js
+++ popup.js
@@ -29,6 +29,10 @@ function getBookmarkParent() {
 
         });
 
+    }).catch(err => {
+
+        reject(err);
+
     });
 
 }
@@ -47,13 +51,107 @@ function createBookmarkGroup(groupName, groupId) {
 
         });
         
+    }).catch(err => {
+
+        reject(err);
+
     });
+
+}
+
+function createTab(url) {
+
+    return (chrome.tabs.create({url: url, active: false}));
+
+}
+
+function createTabGroup(groupName, tabs) {
+
+    var tabIds = [];
+
+    tabs.forEach(tab => {
+
+        tabIds.push(tab.id);
+
+    });
+
+    return new Promise((resolve, reject) => {
+
+        chrome.tabs.group({tabIds: tabIds}).then(groupId => {
+
+            chrome.tabGroups.update(groupId, {title: groupName, collapsed: true}).then(groupUpdate => {
+
+                resolve(groupUpdate);
+
+            });
+
+        }).catch(err => {
+
+            reject(err);
+
+        });
+
+    }).catch(err => {
+
+        reject(err);
+
+    });    
 
 }
 
 function createTabBookmark(tab, bookmarkGroupId) {
 
     return (chrome.bookmarks.create({parentId: bookmarkGroupId, title: tab.title, url: tab.url}));
+
+}
+
+function getAllBookmarks() {
+
+    return new Promise((resolve, reject) => {
+
+        chrome.bookmarks.getTree().then(bookmarkTree => {
+            
+            var otherBookmarks = bookmarkTree[0].children.find(child => child.id == "2");
+            var bookmarkParent = otherBookmarks.children.find(child => child.title == "TabGroupSaver");
+
+            resolve(bookmarkParent.children);
+
+        });
+
+    }).catch(err => {
+
+        reject(err);
+
+    });
+
+
+}
+
+function getBookmark(bookmarkId) {
+
+    return (chrome.bookmarks.get(bookmarkId));
+
+}
+
+function getBookmarkWithChildren(bookmarkId) {
+
+    return new Promise((resolve, reject) => {
+
+        chrome.bookmarks.getTree().then(bookmarkTree => {
+            
+            var otherBookmarks = bookmarkTree[0].children.find(child => child.id == "2");
+            var bookmarkGroups = otherBookmarks.children.find(child => child.title == "TabGroupSaver");
+            var bookmarkGroup = bookmarkGroups.children.find(child => child.id == bookmarkId);
+
+            resolve(bookmarkGroup);
+
+        });
+
+    }).catch(err => {
+
+        reject(err);
+
+    });
 
 }
 
@@ -71,11 +169,6 @@ function getGroup(groupId) {
 
 function getAllTabsInGroup(groupId) {
 
-    // Not working
-    // return (chrome.tabs.query({
-    //     groupId: parseInt(groupId)
-    // }));
-
     return new Promise((resolve, reject) => {
 
         chrome.tabs.query({}).then(tabs => {
@@ -85,6 +178,10 @@ function getAllTabsInGroup(groupId) {
             resolve(tabsInGroup);
 
         });
+
+    }).catch(err => {
+
+        reject(err);
 
     });
 
@@ -120,6 +217,46 @@ function saveGroup() {
 
         });
 
+    }).catch(err => {
+
+        reject(err);
+
+    });
+
+}
+
+function restoreBookmark() {
+
+    return new Promise((resolve, reject) => {        
+
+        getBookmarkWithChildren(this.id).then(bookmarkGroup => {
+
+            Promise.all(bookmarkGroup.children.map(child => {
+
+                return createTab(child.url);
+
+            })).then((tabs) => {
+
+                createTabGroup(bookmarkGroup.title.split(" - ")[0], tabs).then(tabGroup => {
+
+                    chrome.bookmarks.removeTree(bookmarkGroup.id).then(removedBookmarkGroup => {
+
+                        location.reload();
+
+                        resolve();
+
+                    });
+
+                });
+
+            }).catch(err => {
+
+                reject(err);
+    
+            });
+        
+        });                
+
     });
 
 }
@@ -149,6 +286,31 @@ getAllGroups().then(groups => {
         groupDiv.appendChild(groupSaveButton);
         groupSaveButton.appendChild(groupSaveButtonLabel);
         groupsList.appendChild(groupDiv);
+
+    });
+
+});
+
+var bookmarksList = document.getElementById("bookmarksList");
+
+getAllBookmarks().then(bookmarks => {
+
+    bookmarks.forEach(bookmark => {
+
+        var bookmarkDiv = document.createElement("li");
+        var bookmarkName = document.createTextNode(bookmark.title.split(" - ")[0]);
+
+        var bookmarkRestoreButton = document.createElement("button");
+        var bookmarkRestoreButtonLabel = document.createTextNode("Restore");
+
+        bookmarkRestoreButton.id = bookmark.id;
+        bookmarkRestoreButton.addEventListener("click", restoreBookmark);
+
+        bookmarkDiv.appendChild(bookmarkName);
+        bookmarkDiv.appendChild(bookmarkRestoreButton);
+        bookmarkRestoreButton.appendChild(bookmarkRestoreButtonLabel);
+        bookmarksList.appendChild(bookmarkDiv);
+
 
     });
 
